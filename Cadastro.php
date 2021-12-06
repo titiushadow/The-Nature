0d48ce4e3eaaaf66fdc468247e5ef6a558b27404
mesaque <mesaque1212@gmail.com>
2021-12-06 10:42:41 -0200

    beautify

--- Cadastro.php
+++ Cadastro.php
@@ -19,68 +19,72 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
     <title>The Nature</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
 </head>
+
 <body>
 
 
-  
+
     <main class="container">
         <h2>Envie sugestões de animais</h2>
 
-       
-            
+
+
         <form action="cadastro.php" method="POST">
 
             <div class="input-field">
-                <input type="text" name="nome" id="nome"
-                    placeholder="Seu nome">
+                <input type="text" name="nome" id="nome" placeholder="Seu nome">
                 <div class="underline"></div>
             </div>
 
-           
+
 
             <div class="input-field">
-                <input type="Telefone" name="Telefone" id="Telefone"
-                    placeholder="Telefone">
+                <input type="Telefone" name="Telefone" id="Telefone" placeholder="Telefone">
                 <div class="underline"></div>
             </div>
             <br>
-            
+
             <div class="input-field">
-                <input type="E-mail" name="Email" id="Email"
-                    placeholder="E-mail">
+                <input type="E-mail" name="Email" id="Email" placeholder="E-mail">
                 <div class="underline">
                 </div>
             </div>
             <br>
-          
-                <div class="div class="form-group></div>
-                  <label for=""></label>
-                  <textarea class="form-control" placeholder="Envie aqui a sugestão" name="Fale_conosco" id="Fale_conosco" rows="3"></textarea>
-                
-                </div>
-                <div class="underline"></div>
+
+            <div class="div class=" form-group></div>
+            <label for=""></label>
+            <textarea class="form-control" placeholder="Envie aqui a sugestão" name="Fale_conosco" id="Fale_conosco"
+                rows="3"></textarea>
+
+            </div>
+            <div class="underline"></div>
             </div>
             </div>
             <br>
-            <button name="submit" type="submit" class="btn btn-success">Enviar</button> 
+            <button name="submit" type="submit" class="btn btn-success">Enviar</button>
 
         </form>
 
-       
-                      
-                    </a>
-                </div>
-            </div>
+
+
+        </a>
+        </div>
+        </div>
         </div>
     </main>
 </body>
+
 </html>
